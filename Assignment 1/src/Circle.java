3ddeb568e565a0bbcbac7d924ecdcc8dfc8b93df
Marques Swoopes <mswoopes@mail.stmarytx.edu>
2015-02-04 11:26:36 -0600

    Circle project

---
+++ Assignment 1/src/Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+
+	public static void main(String[] args) {
+		
+		int radius = 5;
+		
+		double diameter = radius + radius;
+				
+		double area = radius * radius * Math.PI;
+		
+		System.out.println("The radius of the circle is " + radius);
+		
+		System.out.println("The area of this circle is " + area);
+		
+		System.out.println("The diameter of this circle is " + diameter);
+		
+	}
+}
